9238bec8b3426bf2bf8e34b51e609e99e1301554
Newnius <newnius.cn@gmail.com>
2018-07-12 21:44:32 +0800

    add renew_cert.sh

---
+++ docker/renew_cert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# renew certiticate
+docker run -it --rm --name certbot \
+            -v "/data/ssl/:/etc/letsencrypt" \
+            -v "/data/apache:/var/www/html" \
+            certbot/certbot certonly -n --no-eff-email --email admin@example.com --agree-tos --webroot -w /var/www/html -d example.com
+
+
+# reload nginx
+
+id=$(docker service ps nginx --no-trunc | sed -n '2p' | awk '{print $1}')
+
+name="nginx.1.$id"
+
+docker kill $name
